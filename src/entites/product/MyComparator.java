b35f09589a8be0907fc4db580cc9e6b66cda43e6
victhor409 <victhor409@gmail.com>
2020-02-17 17:25:53 -0300

    Lambda1

---
+++ src/entites/product/MyComparator.java
@@ -0,0 +1,15 @@
+package entites.product;
+
+import java.util.Comparator;
+
+import entites.Product;
+
+public class MyComparator implements Comparator<Product> {
+
+	@Override
+	public int compare(Product p1, Product p2) {
+		return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+	}
+
+	
+}
